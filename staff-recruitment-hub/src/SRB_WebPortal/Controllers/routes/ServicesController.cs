993225b0215820ee49098b3e3806f01a971747c6
Hoàng Khang <hkhang17082005@gmail.com>
2026-03-17 22:17:29 +0700

    1

---
+++ staff-recruitment-hub/src/SRB_WebPortal/Controllers/routes/ServicesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SRB_WebPortal.Controllers.routes
+{
+   public class ServicesController : Controller
+   {
+      public IActionResult Index()
+      {
+         return View();
+      }
+   }
+}
